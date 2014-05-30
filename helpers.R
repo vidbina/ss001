6af77fa13da5400cd48cbaf3ec3b59e335e9a9b2
David Asabina <david@supr.nu>
2014-05-30 18:25:20 +0200

    Separating responsibilities (seperate edge detector + edge indexer)

--- helpers.R
+++ helpers.R
@@ -1,35 +1,47 @@
+#' Detect a step
+#
+# @param s a vector containing at least two numbers
+#' @param thresh size of the delta required to constitute a change
+#' @param rising detect rising edges (true by default)
+#' @param falling detect falling edges (true by default)
+isStep <- function(s, thresh=1, rising=T, falling=T) {
+  if(length(s) < 2) return(F)
+
+  edge <- s[2] - s[1]
+  # TODO: find a concise way to do greater-then-or-equal-to assertions on numbers
+  isStep <- ( 
+    ifelse(rising, (identical(all.equal(edge, thresh), T) || (edge > thresh)), F) 
+    || 
+    ifelse(falling, (identical(all.equal(-edge, thresh), T) || (-edge > thresh)), F) )
+}
+
 #' Count level changes within a series
 #
 #' @param s numberic vector to be counted
 #' @param thresh size of the delta required to constitute a change
 #' @param rising count rising edges
 #' @param falling count falling edges
-countSteps <- function(s, thresh = 1, rising=T, falling=T) {
+countSteps <- function(s, thresh=1, rising=T, falling=T) {
   prevS <- s[1]
   N <- 0
 
   for(currentS in s) {
-    edge <- currentS - prevS
-    # TODO: find a concise way to do greater-then-or-equal-to assertions on numbers
-    isStep <- ( 
-      ifelse(rising, (identical(all.equal(edge, thresh), T) || (edge > thresh)), F) 
-      || 
-      ifelse(falling, (identical(all.equal(-edge, thresh), T) || (-edge > thresh)), F) )
-    N <- N + ifelse(isStep, 1, 0)
+    N <- N + ifelse(isStep(c(prevS, currentS), thresh=thresh, rising=rising, falling=falling), 1, 0)
     prevS <- currentS
   }
   return(N)
 }
 
-#getStepIndex <- function(Nstep, s, thresh = 1, rising=T, falling=T) {
-#  prevS <- s[1]
-#  N <- 0
+
+#' Returns the index of the first post-edge sample 
 #
-#  for(i in length(s)) {
-#    isStep <- ifelse(rising, (s[i] - prevS) >= thresh, F) || ifelse(falling, (prevS - s[i]) >= thresh, F)
-#    N <- N + ifelse(isStep, 1, 0);
-#    prevS <- currentS
-#    if(Nstep == step) return s[i]
-#  }
-#  return(-1)
-#}
+#' Upon specifying a invalid startingpoint startAt zero is returned
+getStepIndex <- function(s, startAt=1, thresh=1, rising=T, falling=T) {
+  if((startAt < 1) || (startAt > (length(s)-1))) return(0)
+  if(length(s) > 1) {
+    for(i in (startAt+1):length(s)) {
+      if(isStep(c(s[i-1], s[i]))) { return(i) }
+    }
+  }
+  return(0)
+}
--- test_helpers.R
+++ test_helpers.R
@@ -1,6 +1,40 @@
-context("Step counter")
-
 source("helpers.R");
+
+context("Edge detector")
+
+test_that("detects all edges of at least a unit amplitude by default", {
+  expect_that(isStep(c(0,2)), equals(T))
+  expect_that(isStep(c(1,0)), equals(T))
+  expect_that(isStep(c(0,0)), equals(F))
+  expect_that(isStep(c(1,1)), equals(F))
+  expect_that(isStep(c(0,0.99)), equals(F))
+})
+
+test_that("detects edges equal or greater to the threshold", {
+  expect_that(isStep(c(0,2), thresh=0.5), equals(T))
+  expect_that(isStep(c(1,0), thresh=0.5), equals(T))
+  expect_that(isStep(c(0,0), thresh=0.5), equals(F))
+  expect_that(isStep(c(1,1), thresh=0.5), equals(F))
+  expect_that(isStep(c(0,0.99), thresh=0.5), equals(T))
+})
+
+test_that("only detects rising edges when falling is set to false", {
+  expect_that(isStep(c(0,2), falling=F), equals(T))
+  expect_that(isStep(c(1,0), falling=F), equals(F))
+  expect_that(isStep(c(0,0), falling=F), equals(F))
+  expect_that(isStep(c(1,1), falling=F), equals(F))
+  expect_that(isStep(c(0,0.99), falling=F), equals(F))
+})
+
+test_that("only detects falling edges when rising is set to false", {
+  expect_that(isStep(c(0,2), rising=F), equals(F))
+  expect_that(isStep(c(1,0), rising=F), equals(T))
+  expect_that(isStep(c(0,0), rising=F), equals(F))
+  expect_that(isStep(c(1,1), rising=F), equals(F))
+  expect_that(isStep(c(0,0.99), rising=F), equals(F))
+})
+
+context("Step counter")
 
 test_that("counts all edges with a minimum delta of 1 unit by default", {
   expect_that(countSteps(c(1,0,1,0,1)), equals(4))
@@ -36,4 +70,33 @@ test_that("has customizable threshold", {
   expect_that(countSteps(c(1,0.5,0.2,0.7,0.1), thresh=0.5, falling=F), equals(1))
   expect_that(countSteps(c(1,0.5,0.2,0.8), thresh=0.6), equals(1))
   expect_that(countSteps(c(1,0.5,0.2,0.8), thresh=1), equals(0))
+})
+
+context("Step indexer")
+
+test_that("picks up the first step in a series of samples", {
+  expect_that(getStepIndex(c(0,1,1,1,0)), equals(2))
+  expect_that(getStepIndex(c(0,0,0,1,1)), equals(4))
+  expect_that(getStepIndex(c(0,0,0,0,0)), equals(0))
+  expect_that(getStepIndex(c(1,1,1,1,1)), equals(0))
+  expect_that(getStepIndex(c(1,1,0,0,0)), equals(3))
+  expect_that(getStepIndex(c(1,0,1,1,1)), equals(2))
+})
+
+test_that("picks up the first step from a defined point in the sample set", {
+  expect_that(getStepIndex(c(0,1,1,1,0), startAt=2), equals(5))
+  expect_that(getStepIndex(c(0,0,0,1,1), startAt=2), equals(4))
+  expect_that(getStepIndex(c(0,0,0,0,0), startAt=2), equals(0))
+  expect_that(getStepIndex(c(1,1,1,1,1), startAt=2), equals(0))
+  expect_that(getStepIndex(c(1,1,0,0,0), startAt=2), equals(3))
+  expect_that(getStepIndex(c(1,0,1,1,1), startAt=2), equals(3))
+})
+
+test_that("returns zero if startAt index out of bounds", {
+  expect_that(getStepIndex(c(0,1,1,1,0), startAt=0), equals(0))
+  expect_that(getStepIndex(c(0,0,0,1,1), startAt=0), equals(0))
+  expect_that(getStepIndex(c(0,0,0,0,0), startAt=5), equals(0))
+  expect_that(getStepIndex(c(1,1,1,1,1), startAt=5), equals(0))
+  expect_that(getStepIndex(c(1,1,1,0,0), startAt=5), equals(0))
+  expect_that(getStepIndex(c(1,0,1,1,1), startAt=5), equals(0))
 })
